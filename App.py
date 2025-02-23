4e4f6a0b093c601bd5460a451ea5c69a9d9d5b46
Thbody32 <jgl08963@uga.edu>
2025-02-22 23:59:56 -0500

    App.py

--- App.py
+++ App.py
@@ -97,10 +97,10 @@ def OptionPrice_FDM_Explicit(S0, K, r, sigma, T, Smax, m, n, option_type):
     return price(S0)
 
 def black_scholes(S, K, T, r, sigma, option_type="call"):
-    d1 = (np.log(S / K) + (r + 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
+    d1 = ((np.log(S / K) + (r + 0.5 * sigma ** 2) * T)) / (sigma * np.sqrt(T))
     d2 = d1 - sigma * np.sqrt(T)
     if option_type == "call":
-        return S * norm.cdf(d1) - K * np.exp(-r * T) * norm.cdf(d2)
+        return S * norm.cdf(d1) - (K * np.exp(-r * T) * norm.cdf(d2))
     else:
         return K * np.exp(-r * T) * norm.cdf(-d1)
 
